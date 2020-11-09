dab22d685dd4e572b94054ba981b9f0851cd9463
khunanan <khunanan.pan@gmail.com>
2020-11-09 17:07:25 +0700

    send homwork5\

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 10)
+            {
+                IHomework05 con = new homewark();
+                Console.Write("Please choose LED to turn On/Off:");
+                String ledNo = Console.ReadLine();
+                string sum = con.DisplayLEDOnScreen(ledNo);
+                Console.WriteLine(sum);
+                Console.WriteLine(" 1  2  3  4  5  6  7  8  9  A");            
+            }
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/homewark.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public class homewark : IHomework05
+    {
+        public static string[] led = { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
+        public string DisplayLEDOnScreen(string ledNo)
+        {
+           
+            string sum = null;
+            if (ledNo == "A" )
+            {
+                led[9] = led[9] == "[ ]" ? "[!]" : "[ ]";
+            }
+            else
+            {
+                int numled = int.Parse(ledNo);
+                numled--;
+                led[numled] = led[numled] == "[ ]" ? "[!]" : "[ ]";
+            }
+            foreach (string item in led)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public string LoadState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCurrentState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAppConfigurations(string onSymbol, string offSymbol, int spacing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
